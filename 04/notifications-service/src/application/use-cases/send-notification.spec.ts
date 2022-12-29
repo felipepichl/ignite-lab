576cacca00535b9eed05f0ccaba8b47ff2775c7e
Felipe Pichl <felipe.pichl@hotmail.com>
2022-12-28 22:46:38 -0300

    04: Doing day: Notification repository in-memory

--- 04/notifications-service/src/application/use-cases/send-notification.spec.ts
+++ 04/notifications-service/src/application/use-cases/send-notification.spec.ts
@@ -1,24 +1,18 @@
-import { Notification } from '../entities/notification';
+import { InMemoryNotificationsRepository } from '../../../test/in-memory-notifications-repository';
 import { SendNotification } from './send-notification';
-
-const notifications: Notification[] = [];
-
-const notificationsRepository = {
-  async create(notification: Notification) {
-    notifications.push(notification);
-  },
-};
 
 describe('Send notification', () => {
   it('should be able to send a new notification', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
     const sendNotification = new SendNotification(notificationsRepository);
 
-    await sendNotification.execute({
+    const { notification } = await sendNotification.execute({
       content: 'This is a notification',
       category: 'social',
       recipientId: 'recipient_id',
     });
 
-    expect(notifications).toHaveLength(1);
+    expect(notificationsRepository.notifications).toHaveLength(1);
+    expect(notificationsRepository.notifications[0]).toEqual(notification);
   });
 });
