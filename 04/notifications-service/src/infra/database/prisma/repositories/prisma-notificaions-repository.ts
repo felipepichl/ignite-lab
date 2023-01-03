91be6548e99b765c357accca673e4ce38addc4c2
Felipe Pichl <felipe.pichl@hotmail.com>
2023-01-02 22:01:00 -0300

    04: Doing day: Prisma Repository

---
+++ 04/notifications-service/src/infra/database/prisma/repositories/prisma-notificaions-repository.ts
@@ -0,0 +1,29 @@
+import { Notification } from 'src/application/entities/notification';
+import { NotificationsRepository } from '../../../../application/repositories/notifications-repository';
+import { PrismaService } from '../prisma.service';
+
+class PrismaNotificationsRepository implements NotificationsRepository {
+  constructor(private prismaService: PrismaService) {}
+
+  async create({
+    id,
+    category,
+    content,
+    recipientId,
+    readAt,
+    createdAt,
+  }: Notification): Promise<void> {
+    await this.prismaService.notification.create({
+      data: {
+        id,
+        category,
+        content: content.value,
+        recipientId,
+        readAt,
+        createdAt,
+      },
+    });
+  }
+}
+
+export { PrismaNotificationsRepository };
