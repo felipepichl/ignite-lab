a9e197cffdcfc6648109f7316033a197cdec72fe
Felipe Pichl <felipe.pichl@hotmail.com>
2023-01-02 22:20:50 -0300

    04: Doing day 01: Done

--- 04/notifications-service/src/infra/http/http.module.ts
+++ 04/notifications-service/src/infra/http/http.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { SendNotification } from '../../application/use-cases/send-notification';
+import { DatabaseModule } from '../database/database.module';
 import { NotificationController } from '../http/controller/notifications.controller';
 
 @Module({
-  imports: [],
+  imports: [DatabaseModule],
   controllers: [NotificationController],
+  providers: [SendNotification],
 })
 export class HttpModule {}
