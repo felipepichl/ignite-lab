9432d6d7c3a7b12e2a13b81886a0fd2454488c7f
Felipe Pichl <felipe.pichl@hotmail.com>
2023-01-22 22:43:12 -0300

    04: Day 03: 25:10

--- 04/notifications-service/src/application/repositories/notifications-repository.ts
+++ 04/notifications-service/src/application/repositories/notifications-repository.ts
@@ -2,6 +2,7 @@ import { Notification } from '../entities/notification';
 
 abstract class NotificationsRepository {
   abstract create(notification: Notification): Promise<void>;
+  abstract findById(notificationId: string): Promise<Notification | null>;
 }
 
 export { NotificationsRepository };
