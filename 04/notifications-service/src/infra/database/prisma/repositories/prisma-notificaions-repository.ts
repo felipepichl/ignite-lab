948f1f3e81e8189118871cc56171c822bb546f0d
Felipe Pichl <felipe.pichl@hotmail.com>
2023-01-29 23:27:33 -0300

    04: Day 03: Doing cancel notification test

--- 04/notifications-service/src/infra/database/prisma/repositories/prisma-notificaions-repository.ts
+++ 04/notifications-service/src/infra/database/prisma/repositories/prisma-notificaions-repository.ts
@@ -8,12 +8,20 @@ import { PrismaNotificationMapper } from '../mappers/prisma-notification-mapper'
 class PrismaNotificationsRepository implements NotificationsRepository {
   constructor(private prismaService: PrismaService) {}
 
+  async findById(notificationId: string): Promise<Notification | null> {
+    throw new Error('Method not implemented.');
+  }
+
   async create(notification: Notification): Promise<void> {
     const raw = PrismaNotificationMapper.toPrisma(notification);
 
     await this.prismaService.notification.create({
       data: raw,
     });
+  }
+
+  async save(notification: Notification): Promise<void> {
+    throw new Error('Method not implemented.');
   }
 }
 
